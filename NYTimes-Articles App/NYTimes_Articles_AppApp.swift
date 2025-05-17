25eeccc6204c9029749b477439e30efd0ddfe81c
nF <naalmuaither@elm.sa>
2025-05-17 11:41:31 +0300

    Initial Commit

---
+++ NYTimes-Articles App/NYTimes_Articles_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTimes_Articles_AppApp.swift
+//  NYTimes-Articles App
+//
+//  Created by Nawaf  on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct NYTimes_Articles_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
