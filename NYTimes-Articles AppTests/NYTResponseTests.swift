4044196ac897ed0a3624734ee3899402c4af5335
nF <naalmuaither@elm.sa>
2025-05-17 12:36:07 +0300

    Add test for decoding valid data

---
+++ NYTimes-Articles AppTests/NYTResponseTests.swift
@@ -0,0 +1,20 @@
+//
+//  NYTResponseTests.swift
+//  NYTimes-Articles AppTests
+//
+//  Created by Nawaf  on 17/05/2025.
+//
+
+import Foundation
+@testable import NYTimes_Articles_App
+import XCTest
+
+final class NYTResponseTests: XCTestCase {
+    func test_decode_validData_succeeds() throws {
+        let data = try MockFactory.loadJSON(named: "article_response")
+        let response = try JSONDecoder().decode(NYTResponse.self, from: data)
+        
+        XCTAssertFalse(response.results.isEmpty)
+        XCTAssertEqual(response.results.first?.id, 1234567890)
+    }
+}
